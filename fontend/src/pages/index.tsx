92fada5691b1f3baed389dd845fe4f35b8d14cc9
Taketo Yahiro (Yapimaru) <yahiro.t.eng@gmail.com>
2023-07-12 20:26:58 +0900

    websocketと接続し、メッセージの送信ができるように

--- fontend/src/pages/index.tsx
+++ fontend/src/pages/index.tsx
@@ -1,28 +1,80 @@
 import { Layout } from "@/components/Layout"
 import { MessageItem } from "@/components/MessageItem"
+import { SystemMessageItem } from "@/components/SystemMessageItem"
+import { Message } from "@/types/Message"
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from "react"
 
 export default function Home() {
+    const [userName, setUserName] = useState<string>("")
+    const [message, setMessage] = useState<string>("")
+    const [messages, setMessages] = useState<Message[]>([])
+    const socketRef = useRef<WebSocket>()
+
+    useEffect(() => {
+        const name = window.prompt("Enter your name") as string
+        setUserName(name)
+
+        socketRef.current = new WebSocket("ws://localhost:8000/ws/")
+        socketRef.current.onopen = () => {
+            console.log("connected")
+
+            socketRef.current?.send(
+                JSON.stringify({
+                    message: `${name} enter room!`,
+                })
+            )
+        }
+
+        socketRef.current.onmessage = (e) => {
+            const data = JSON.parse(e.data)
+            //console.log(messages)
+            setMessages((prevData) => [...prevData, data])
+        }
+
+        return () => {
+            if (!socketRef.current) return
+
+            socketRef.current.close()
+        }
+    }, [])
+
+    const sendMessage = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (!socketRef.current) return
+        setMessage("")
+        socketRef.current.send(
+            JSON.stringify({
+                name: userName,
+                message,
+            })
+        )
+    }
+
     return (
         <Layout>
             <div className="flex w-full h-full">
                 <div className="w-md max-w-base bg-white rounded-lg mx-auto p-3 md:p-8">
                     <ul className="overflow-y-auto h-full flex flex-col gap-3 md:gap-5">
-                        <MessageItem
-                            isMine={true}
-                            name="test"
-                            message="Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test"
-                        />
-                        <MessageItem
-                            isMine={false}
-                            name="test"
-                            message="Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test"
-                        />
+                        {messages &&
+                            messages.map((msgItem, i) => {
+                                if (msgItem.name) {
+                                    return <MessageItem key={i} name={msgItem.name} message={msgItem.message} isMine={msgItem.name == userName} />
+                                } else {
+                                    return <SystemMessageItem key={i} message={msgItem.message} />
+                                }
+                            })}
                     </ul>
                 </div>
                 <div className="fixed bottom-0 left-0 right-0 ">
-                    <form className="flex">
-                        <input className="w-24 text-sm md:text-base md:w-80 border border-black p-2 font-medium" type="text" placeholder="your name" />
-                        <input className="flex-1 border border-black text-lg p-2 font-medium" placeholder="enter message" type="text" />
+                    <form onSubmit={(e) => sendMessage(e)} className="flex">
+                        {/* <input className="w-24 text-sm md:text-base md:w-80 border border-black p-2 font-medium" type="text" placeholder="your name" /> */}
+                        <input
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => setMessage(e.target.value)}
+                            className="flex-1 border border-black text-lg p-2 font-medium"
+                            placeholder="enter message"
+                            type="text"
+                            value={message}
+                        />
                         <input className="bg-emerald-600 text-white border border-black px-2 md:px-8 hover:opacity-80 transition-opacity cursor-pointer" type="submit" />
                     </form>
                 </div>
