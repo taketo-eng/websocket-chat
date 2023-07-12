fad80551371256abca1ebeaf6b4fe80c3706fff9
Taketo Yahiro (Yapimaru) <yahiro.t.eng@gmail.com>
2023-07-13 03:38:57 +0900

    複数のクライアントに対応化

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 import json
+from typing import List
 
 app = FastAPI()
 
@@ -17,16 +18,50 @@ app.add_middleware(
     allow_headers=['*']
 )
 
-@app.websocket('/ws')
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        try:
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, data, websocket: WebSocket, client_name:str = None):
+        await websocket.send_json({
+            'message': data['message'],
+            'name': client_name,
+            'sendTime': data['sendTime']
+        })
+
+    async def broadcast(self, data, websocket: WebSocket, client_name: str = None):
+
+        for connection in self.active_connections:
+            if websocket != connection:
+                await connection.send_json({
+                    'message': data['message'],
+                    'name': client_name,
+                    'sendTime': data['sendTime']
+                })
+
+manager = ConnectionManager()
+
+@app.websocket('/ws/{client_name}')
+async def websocket_endpoint(websocket: WebSocket, client_name: str):
+    await manager.connect(websocket)
+    try:
+        while True:
             json_data = await websocket.receive_text()
             data = json.loads(json_data)
-            print(data)
-            await websocket.send_json(data)
-        except Exception as e:
-            print('error', e)
-            break
+            if data['messageType'] == 'system':
+                await manager.send_personal_message(data, websocket)
+                await manager.broadcast(data)
+            else:
+                await manager.send_personal_message(data, websocket, client_name)
+                await manager.broadcast(data, websocket, client_name)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast({"message": f'{client_name} left the room'})
     print('Bye Bye')
